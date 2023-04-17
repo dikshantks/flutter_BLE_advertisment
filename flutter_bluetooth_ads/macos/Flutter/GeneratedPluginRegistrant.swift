d35e77042f63b1200bc30613707fd152bf6b74c6
dikshant <dikshantks229@gmail.com>
2023-04-17 13:27:54 +0530

    adding admin panel

--- flutter_bluetooth_ads/macos/Flutter/GeneratedPluginRegistrant.swift
+++ flutter_bluetooth_ads/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -10,7 +10,7 @@ import firebase_core
 import firebase_storage
 import flutter_blue
 import flutter_local_notifications
-import path_provider_macos
+import path_provider_foundation
 import sqflite
 import url_launcher_macos
 
