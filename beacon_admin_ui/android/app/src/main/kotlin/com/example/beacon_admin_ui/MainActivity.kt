c48187fcabe76776b5c0434cbd641bceff93e721
dikshant <dikshantks229@gmail.com>
2023-04-28 16:38:23 +0530

    cleaner code

--- beacon_admin_ui/android/app/src/main/kotlin/com/example/beacon_admin_ui/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.beacon_admin_ui
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- beacon_admin_ui/lib/constants.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-const kprimary = Color(0xff42BEA5);
-const ksecondary = Color(0xff359F8A);
-const kptext = Color(0xff1A1F24);
-const kstext = Color(0xff95A1AC);
-
-const defaultPadding = 20.0;
